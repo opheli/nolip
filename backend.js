ac3dbf71f8eec506f0b0a6c2cc4a0bbd026426a9
ophélie <ophelieleleu@yahoo.fr>
2021-08-13 14:03:36 +0200

    routes à revoir

--- backend.js
+++ backend.js
@@ -2,11 +2,14 @@ const express = require('express');
 const routes = require('./backend/controllers/routes')
 const cors = require('cors');
 const app = express();
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/nolip',
-{ useNewUrlParser: true, useUnifiedTopology: true })
+const mongoose = require('mongoose')
+const mongo_uri = process.env.MONGO || 'mongodb://localhost:27017/nolip'
+mongoose.connect(mongo_uri,
+     { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.info('✔ Connected to MongoDB'),
+)
 
-const port = 8000;
+const port =  process.env.PORT || 8000;
 
 app.listen(port, () => {
     console.info('server started on port : ' + port)
@@ -14,8 +17,7 @@ app.listen(port, () => {
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
-app.use('/', routes);
 app.use(express.static('build'));
+app.use('/', routes);
 
 
